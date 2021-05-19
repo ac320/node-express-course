e92c864927da8cce5bdc0aecf68d0f65f934d87e
ac320 <acflies@protonmail.com>
2021-05-18 17:17:13 -0700

    add dynamic GET routine

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const mockUserData=[
 ]
 
 app.get('/users',function(req,res){
-      res.json({
+        res.json({
               success: true,
               message: 'successfilly got users. Nice!',
               users: mockUserData
-      })
+        })
 })
 
+app.get('/users/:id',function(req,res){
+        console.log(req.params.idea)
+        res.json({
+                success: true,
+                message: 'got one user',
+                user: req.params.id
+        })
+})
 
 app.listen(8000,function(){console.log("server is running")})
